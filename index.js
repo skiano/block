1da316dcf86290d69a8e8ff119821f1b813a1c77
Greg Skiano <gregoryskiano@gmail.com>
2015-02-21 17:20:57 -0500

    making space for tests

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var block = require('./Block');
 
 var b = block([100, 100]);
 
-var p = b.get(0,0);
+var p = b.get(1,1);
 
 console.log(p);
---
+++ test/block.test.js
@@ -0,0 +1,12 @@
+
+var should = require('should');
+
+describe('Block', function(){
+
+  it('should do something', function(){
+    
+    (5).should.be.exactly(5).and.be.a.Number;
+
+  });
+
+});
