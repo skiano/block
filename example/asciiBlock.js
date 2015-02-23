35fa8e79fd92c92163c715d5ab763a1f9d36d60b
Greg Skiano <gregoryskiano@gmail.com>
2015-02-22 21:08:55 -0500

    playing with motifs

--- example/asciiBlock.js
+++ example/asciiBlock.js
@@ -1,5 +1,4 @@
 var block = require('../block'),
-  rand = require('random-seed').create(),
   VOID = ' ',
   DEFAULT_GLYPH = 'X';
 
@@ -7,6 +6,9 @@ function AsciiBlock (dimensions, options) {
 
   var b = block(dimensions, options),
     marks = {};
+
+  this.width = dimensions[0];
+  this.height = dimensions[1];
 
   this.mark = function (point, glyph) {
     glyph = glyph || DEFAULT_GLYPH;
@@ -40,32 +42,96 @@ function AsciiBlock (dimensions, options) {
 
 }
 
-var pen = function (point) {
-  return [
-    point[0] + 1,
-    point[1] + 1,
-  ];
-};
+// TESTING
 
-var a = new AsciiBlock([14,13], {
-  step: 3,
-  direction: 'y'
+function addPoints (A, B) {
+  return [A[0] + B[0], A[1] + B[1]];
+}
+
+var rand = require('random-seed').create();
+
+var a = new AsciiBlock([
+    rand.intBetween(15,51),
+    rand.intBetween(15,51)
+  ], {
+    step: rand.intBetween(2,25),
+    direction: ['x','y'][rand.intBetween(0,1)]
 });
 
-// var i;
-// for (i = 0; i < 10; i += 1) {
-//   a.mark([0,1], 'X');
-// }
+var randMark = (function () {
+  var marks = '..*'.split('');
+  return function () {
+    return marks[rand.intBetween(0,marks.length - 1)];
+  };
+})();
 
-a.mark([0,1], 'X');
-a.mark([2,2], 'O');
-a.mark([3,0], '_');
-a.mark([2,0], '_');
-a.mark([1,0], '_');
-a.mark([-1,0], '_');
+var i;
+for (i = 0; i < rand.intBetween(43,700); i += 1) {
+  var p = [rand.intBetween(0,a.width), rand.intBetween(0,a.height)];
+  a.mark(p, randMark());
+}
 
-var str = a.render(100,20, 0, -2);
-console.log(str);
+function cross (point, block) {
+  block.mark(point, '-');
+  block.mark(addPoints(point,[0,1] ), '|');
+  block.mark(addPoints(point,[0,-1]), '|');
+  block.mark(addPoints(point,[0,2]), '|');
+  block.mark(addPoints(point,[1,0] ), '-');
+  block.mark(addPoints(point,[2,0]), '-');
+  block.mark(addPoints(point,[-1,0]), '-');
+  block.mark(addPoints(point,[-2,0] ), '-');
+}
+
+for (i = 0; i < rand.intBetween(3, 10); i += 1) {
+  var p = [rand.intBetween(0,a.width), rand.intBetween(0,a.height)];
+  cross(p, a);
+}
+
+function xMark (point, block) {
+  block.mark(point, 'X');
+  block.mark(addPoints(point,[1,1] ), 'X');
+  block.mark(addPoints(point,[-1,-1] ), 'X');
+  block.mark(addPoints(point,[1,-1] ), 'X');
+  block.mark(addPoints(point,[-1,1] ), 'X');
+}
+
+for (i = 0; i < rand.intBetween(1, 15); i += 1) {
+  var p = [rand.intBetween(0,a.width), rand.intBetween(0,a.height)];
+  xMark(p, a);
+}
+
+function box (point, block, w, h) {
+
+  var x, y;
+  
+  for (x = 0; x <= w; x += 1) {
+    block.mark(addPoints(point,[x,h]), '\\');
+    block.mark(addPoints(point,[x,0]), '\\');
+  }
+
+  // for (y = 0; y <= h; y += 1) {
+  //   block.mark(addPoints(point,[w,y]), '|');
+  //   block.mark(addPoints(point,[0,y]), '|');
+  // }
+
+  // empty out center
+  for (y = 1; y < h; y += 1) {
+    for (x = 0; x <= w; x += 1) {
+      block.mark(addPoints(point, [x,y]), '/');
+    }
+  }
+
+}
+
+for (i = 0; i < rand.intBetween(1, 1); i += 1) {
+  var p = [rand.intBetween(0,a.width), rand.intBetween(0,a.height)];
+  var w = rand.intBetween(3,7);
+  var h = rand.intBetween(3,7);
+  box(p, a, w, h);
+}
+
+var str = a.render(140,40, 0, -2);
+console.log('\n' + str + '\n');
 
 
 
